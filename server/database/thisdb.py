29ff3cbb02d9a02c13170760406c9b390c783ce8
Dustin Lee <dustidl1@uci.edu>
2024-12-28 09:27:05 -0800

    fixing the table so it auto increments

--- server/database/thisdb.py
+++ server/database/thisdb.py
@@ -3,13 +3,16 @@ import sqlite3
 con = sqlite3.connect("server/database/recommendations.db")
 cursor = con.cursor()
 
-users_column = """ CREATE TABLE IF NOT EXISTS  users(
-            id INT PRIMARY KEY,
-            username VARCHAR(225) UNIQUE NOT NULL,
-            hashed_pass VARCHAR(225) NOT NULL
-        ); """
+users_column = """ 
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username VARCHAR(225) UNIQUE NOT NULL,
+        hashed_pass VARCHAR(225) NOT NULL
+    ); """
 
 cursor.execute(users_column)
+
+# cursor.execute("DROP TABLE users")
 
 
 con.commit()
